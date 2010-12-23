5b395e93342de946dedafb754e393f67661bfa6d
Dmitry Khromov <devandart@ya.ru>
2010-12-23 03:30:25 +0300

    fix sort for comments on main page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 
-  scope :main_page, :limit => 10
+  scope :main_page, :limit => 10, :order => "created_at DESC"
   acts_as_commentable
 
 end
--- db/sample/post.rb
+++ db/sample/post.rb
@@ -4,4 +4,5 @@ require "faker"
 Post.populate(20) do |p|
   p.subject = Faker::Lorem.words(10).join(" ")
   p.body = Faker::Lorem.paragraph
+  p.created_at = (rand(9) + 1).day.ago
 end
