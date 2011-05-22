aab2167402d79ef973095cdd2ed67e6a51ca8dc4
Diman <devandart@ya.ru>
2011-05-22 11:35:23 +0400

    Ссылки в меню переключаются

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def class_for_element_menu(name)
+    if params[:controller] == "home" && name == t("link.main_page")
+      "active"
+    elsif params[:controller] == "profiles" && name == t("link.profile")
+      "active"
+    elsif params[:controller] == "posts" && !params[:type].blank? && name == t("link.my_posts")
+      "active"
+    elsif params[:controller] == "posts" && params[:action] == "new" && name == t("link.create_post")
+      "active"
+    else
+      "no_active"
+    end
+  end
 end
